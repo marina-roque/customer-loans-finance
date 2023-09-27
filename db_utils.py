7c533e9fc1592a79e42aeeda1463608275932491
Marina Roque <marinaelena_11@hotmail.com>
2023-09-27 21:56:12 +0100

    Changed all the code after running tests

--- db_utils.py
+++ db_utils.py
@@ -1,67 +1,85 @@
 #%%
 import pandas as pd
-import yaml
 from sqlalchemy import create_engine
+import yaml
+import psycopg2
+import csv
 
+# Load the database credentials from a YAML file
 class RDSDatabaseConnector:
-    def __init__(self, credentials_file_path):
-        self.credentials_file_path = credentials_file_path
-        self.load_credentials()
-        self.initialize_engine()
-
-    def load_credentials(self):
-        # Load database credentials from the YAML file
-        try:
-            with open(self.credentials_file_path, 'r') as file:
-                self.credentials = yaml.safe_load(file)
-        except Exception as e:
-            print(f"Error loading credentials from {self.credentials_file_path}: {str(e)}")
-            return None
+    def __init__(self, credentials_file):
+        self.credentials = self.load_credentials(credentials_file)
+        self.connection = self.connect_to_database(self.credentials)
+        self.db_engine = self.create_db_engine(self.credentials)
         
-    def initialize_engine(self):
-        # Initialize the SQLAlchemy engine.
+    def load_credentials(self, file_path):
         try:
-            db_url = f"postgresql+psycopg2://{self.credentials['RDS_USER']}:{self.credentials['RDS_PASSWORD']}@{self.credentials['RDS_HOST']}:{self.credentials['RDS_PORT']}/{self.credentials['RDS_DATABASE']}"
-            self.engine = create_engine(db_url)
+            with open(file_path, 'r') as file:
+                credentials = yaml.safe_load(file)
+            return credentials
+        except Exception as e:
+            print(f"Error loading credentials from {file_path}: {str(e)}")
+            return None
+
+    # Connect to the remote database
+    def connect_to_database(self, credentials):
+        try:
+            connection = psycopg2.connect(
+                host=credentials['RDS_HOST'],
+                port=credentials['RDS_PORT'],
+                database=credentials['RDS_DATABASE'],
+                user=credentials['RDS_USER'],
+                password=credentials['RDS_PASSWORD']
+            )
+            return connection
+        except Exception as e:
+            print(f"Error connecting to the database: {str(e)}")
+            return None
+
+    # Create the SQLAlchemy engine
+    def create_db_engine(self, credentials):
+        try:
+            engine = create_engine(f"postgresql://{credentials['RDS_USER']}:{credentials['RDS_PASSWORD']}@{credentials['RDS_HOST']}:{credentials['RDS_PORT']}/{credentials['RDS_DATABASE']}")
+            return engine
         except Exception as e:
             print(f"Error creating database engine: {str(e)}")
             return None
         
-    def extract_data(self, loan_payments):
-        # Extract data from the specified table and return it as a Pandas DataFrame.
-        try:
-            query = f'SELECT * FROM {loan_payments}'
-            data = pd.read_sql_query(query, self.engine)
-            return data
-        except Exception as e:
-            print(f"Error extracting data from {loan_payments}: {str(e)}")
-            return None
+    def extract_data_to_csv(self, table_name, csv_file_path):
+        if not self.connection or not self.db_engine:
+            print("Database connection or engine not available. Exiting.")
+            return
+
+        cursor = self.connection.cursor()
         
-    def save_to_csv(self, data, file_path):
         try:
-            data.to_csv(file_path, index=False)
+            query = f'SELECT * FROM {table_name}'
+            cursor.execute(query)
+            data = cursor.fetchall()
         except Exception as e:
-            print(f"Error saving data to {file_path}: {str(e)}")
-            return None
-        
+            print(f"Error executing SQL query: {str(e)}")
+            return
+
+        try:
+            with open(csv_file_path, 'w', newline='') as csv_file:
+                csv_writer = csv.writer(csv_file)
+                csv_writer.writerow([desc[0] for desc in cursor.description])  # Write column headers
+                csv_writer.writerows(data)  # Write data rows
+                print(f"Data saved to {csv_file_path}")
+        except Exception as e:
+            print(f"Error saving data to CSV file: {str(e)}")
+        finally:
+            cursor.close()
+            self.connection.close()
+
 if __name__ == "__main__":
-    # Step 1: Load the credentials from the YAML file
-    credentials_file_path = 'credentials.yaml'
-
-    # Step 2: Create an instance of the RDSDatabaseConnector class
-    db_connector = RDSDatabaseConnector(credentials_file_path)
-
-    # Step 3: Replace 'your_table_name' with the name of the table you want to extract data from
-    table_name = 'loans_payments'
-
-    # Step 4: Extract data from the specified table
-    data = db_connector.extract_data(table_name)
-
-    # Step 5: Specify the file path where you want to save the CSV file
-    csv_file_path = 'loans_payments.csv'
-
-    # Step 6: Save the data to a CSV file
-    db_connector.save_to_csv(data, csv_file_path)
+    # Initialize the RDSDatabaseConnector and specify the table and CSV file path
+    connector = RDSDatabaseConnector('credentials.yaml')
+    table_name = 'loan_payments'
+    csv_file_path = 'loan_payments.csv'
+    
+    # Extract data and save it to a CSV file
+    connector.extract_data_to_csv(table_name, csv_file_path)
 
 
 
